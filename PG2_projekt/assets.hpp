5d49b1e7dd1e079e7a8814de210e75deb8459823
Jan_Schejbal <jan.schejbal@tul.cz>
2025-03-13 13:17:36 +0100

    Zacatek cv04

--- PG2_projekt/assets.hpp
+++ PG2_projekt/assets.hpp
@@ -1,11 +1,9 @@
 #pragma once
-#include <GL/glew.h> 
-#include <GL/wglew.h> 
-#include <glm/glm.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <vector>
 
-// Vertex description
-struct vertex {
-    glm::vec3 position;
+#include <glm/glm.hpp> 
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
 };
